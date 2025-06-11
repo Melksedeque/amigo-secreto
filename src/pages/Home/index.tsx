e5d6deb4b0a0398895d3b1a3b60728b42c123ca2
Melksedeque <freelancer@melksedeque.com.br>
2025-06-11 19:24:30 -0300

    Criação de página Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,57 @@
+import Formulario from 'components/Formulario'
+import styles from './Home.module.scss'
+import Header from 'components/Header'
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+      <h2>Vamos começar!</h2>
+      <Formulario />
+      <ul>
+        <li>João</li>
+        <li>Maria</li>
+        <li>José</li>
+        <li>Lúcia</li>
+      </ul>
+      <footer>
+        <div>
+          <a
+            href="https://www.linkedin.com/in/melksedeque-silva/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+          <a href="http://github.com/Melksedeque" target="_blank">
+            GitHub
+          </a>
+          <a href="https://x.com/SouzaMelk" target="_blank">
+            Twitter
+          </a>
+        </div>
+        <div>
+          <p>
+            Desenhado por{' '}
+            <a
+              href="http://www.alura.com.br/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Alura
+            </a>
+          </p>
+          <p>
+            Desenvolvido por{' '}
+            <a
+              href="http://github.com/Melksedeque"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Melksedeque
+            </a>
+          </p>
+        </div>
+      </footer>
+    </>
+  )
+}
