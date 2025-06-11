38a8a128b053f799f18870324e127f4649277d36
Melksedeque <freelancer@melksedeque.com.br>
2025-06-11 19:54:49 -0300

    Criação de lógica no formulário com hook personalizado para adicionar nome à lista

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,33 @@
+import { useRef, useState } from 'react'
 import styles from './Formulario.module.scss'
+import { useAdicionarParticipante } from 'hooks/useAdicionarParticipante';
 
 export default function Formulario() {
+  const [nome, setNome] = useState('');
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const adicionarNaLista = useAdicionarParticipante()
+
+  const adicionarParticipante = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    adicionarNaLista(nome)
+    setNome('');
+    inputRef.current?.focus()
+  }
+
   return (
     <div className={styles.container}>
       <h1>Formulario</h1>
-      <form>
-        <input type="text" placeholder="Insira os nomes dos participantes" />
-        <button disabled={true}>Adicionar</button>
+      <form onSubmit={adicionarParticipante}>
+        <input
+          type="text"
+          value={nome}
+          ref={inputRef}
+          onChange={e => setNome(e.target.value)}
+          placeholder="Insira os nomes dos participantes"
+        />
+        <button disabled={!nome}>Adicionar</button>
       </form>
     </div>
   )
