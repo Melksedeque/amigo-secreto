1e532e6b8c9d5bc7df244e57183a333b0e02cd73
Melksedeque <freelancer@melksedeque.com.br>
2025-06-11 19:08:46 -0300

    Componente de logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import styles from './Logo.module.scss'
 export default function Logo() {
   return (
     <div className={styles.logo}>
-      <img src="/images/logo.png" alt="Logo do Sorteador de Amigo Secreto" />
+      <h1><img src="/images/logo.png" alt="Logo do Sorteador de Amigo Secreto" /></h1>
     </div>
   )
 }
