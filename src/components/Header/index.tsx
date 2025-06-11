98c838c4e417b88ae1cbd862da891e4cb3aa4fdd
Melksedeque <freelancer@melksedeque.com.br>
2025-06-11 18:35:08 -0300

    Criação de componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.scss'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Sorteio de Amigo Secreto</h1>
+    </header>
+  )
+}
