aec9cce7e2097b702ba575a60573ace46159521b
Melksedeque <freelancer@melksedeque.com.br>
2025-06-11 19:55:21 -0300

    Testes de adicionar nome na lista

--- src/components/Formulario/Formulario.test.tsx
+++ src/components/Formulario/Formulario.test.tsx
@@ -1,6 +1,7 @@
 import Formulario from ".";
 import { describe, expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
 
 describe('Formulario', () => {
     test('quando o input está vazio, novos participantes não podem ser adicionados', () =>{
@@ -9,6 +10,22 @@ describe('Formulario', () => {
         const botao = screen.getByRole('button')
     
         expect(input).toBeInTheDocument()
+        expect(botao).toBeDisabled()
+    })
+    test('deve adicionar um participante caso exista um nome preenchido', () => {
+        render(<RecoilRoot><Formulario /></RecoilRoot>)
+        const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+        const botao = screen.getByRole('button')
+
+        fireEvent.change(input, {
+            target: {
+                value: 'João'
+            }
+        })
+        fireEvent.click(botao)
+
+        expect(input).toHaveFocus()
+        expect(input).toHaveValue('')
         expect(botao).toBeDisabled()
     })
 })
