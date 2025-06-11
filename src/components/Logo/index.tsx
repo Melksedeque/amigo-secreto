4c8b92ef31dd5827c23c375a398577009afd631c
Melksedeque <freelancer@melksedeque.com.br>
2025-06-11 19:13:53 -0300

    Estrutura inicial de Logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import styles from './Logo.module.scss'
 export default function Logo() {
   return (
     <div className={styles.logo}>
-      <h1><img src="/images/logo.png" alt="Logo do Sorteador de Amigo Secreto" /></h1>
+      <h1><img src="/images/logo-pequeno.png" alt="Logo do Sorteador de Amigo Secreto" /></h1>
     </div>
   )
 }
