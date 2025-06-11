a18febc0bd0c140232a6b2ea47c1c0ce4cac55ee
Melksedeque <freelancer@melksedeque.com.br>
2025-06-11 18:51:27 -0300

    Configuração de paths relativos do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,36 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, './src/components'),
+      pages: path.resolve(__dirname, './src/pages'),
+      styles: path.resolve(__dirname, './src/styles'),
+      types: path.resolve(__dirname, './src/types'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
     css: false,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "styles/_breakpoints.scss" as *;
+        `,
+      },
+    },
   },
 })
