a13656885f9efc80376ee43e0db767089dfa052a
Melksedeque <freelancer@melksedeque.com.br>
2025-06-11 20:14:35 -0300

    Define mensagem de erro caso tente adicionar nomes duplicados

--- src/state/hooks/useAdicionarParticipante.ts
+++ src/state/hooks/useAdicionarParticipante.ts
@@ -1,10 +1,16 @@
-import { useSetRecoilState } from "recoil";
-import { listaParticipantesState } from "../atom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { erroState, listaParticipantesState } from "../atom";
 
 export const useAdicionarParticipante = () => {
     const setLista = useSetRecoilState(listaParticipantesState);
+    const lista = useRecoilValue(listaParticipantesState);
+    const setErro = useSetRecoilState(erroState);
     return (nomeDoParticipante: string) => {
         if (nomeDoParticipante.length > 3) {
+            if(lista.includes(nomeDoParticipante)) {
+                setErro("Nomes duplicados não são permitidos!");
+                return;
+            }
             return setLista(listaAtual => [...listaAtual, nomeDoParticipante]);
         } else {
             alert("Nome do participante precisa ter mais de 3 caracteres");
