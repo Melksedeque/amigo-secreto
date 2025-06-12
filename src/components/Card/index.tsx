edf526787f3e5d9108c55c78106a71940498b2a0
Melksedeque <freelancer@melksedeque.com.br>
2025-06-12 20:32:07 -0300

    Adição de titulo ao Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,6 +3,7 @@ import styles from './Card.module.scss'
 export default function Card({children}: React.PropsWithChildren) {
     return (
         <div className={styles.card}>
+            <h2>Vamos começar!</h2>
             {children}
         </div>
     )
