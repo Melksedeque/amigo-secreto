9fb6c448067f53dd37d08ab12c689064dc11c2e9
Melksedeque <freelancer@melksedeque.com.br>
2025-06-12 21:25:30 -0300

    Criação de teste com dados mockados de lista vazia e lista preenchida

--- src/components/ListaParticipantes/ListaParticipantes.test.tsx
+++ src/components/ListaParticipantes/ListaParticipantes.test.tsx
@@ -1,16 +1,41 @@
 import { RecoilRoot } from "recoil";
-import { describe, expect, test } from "vitest";
+import { beforeEach, describe, expect, test, vitest, type MockedFunction } from "vitest";
 import ListaParticipantes from ".";
 import { render, screen } from "@testing-library/react";
+import { useListaDeParticipantes } from "hooks/useListaDeParticipantes";
+
+vitest.mock('hooks/useListaDeParticipantes', () => {
+    return {
+        useListaDeParticipantes: vitest.fn()
+    }
+})
+
+const mockedUseListaDeParticipantes = useListaDeParticipantes as MockedFunction<typeof useListaDeParticipantes>
 
 describe('Comportamento do componente ListaParticipantes', () => {
+    beforeEach(() => {
+        mockedUseListaDeParticipantes.mockClear()
+    })
+
     test('Deve renderizar o componente', () => {
         render(<RecoilRoot><ListaParticipantes /></RecoilRoot>)
         expect(screen.getByTestId('listaParticipantes')).toBeInTheDocument()
     })
-    test('Lista de itens deve ter 3 itens', () => {
+    test('Lista deve ter um título', () => {
         render(<RecoilRoot><ListaParticipantes /></RecoilRoot>)
-        const itens = screen.queryAllByRole('listitem')
-        expect(itens).toHaveLength(3)
+        expect(screen.getByTitle('Participantes')).toBeInTheDocument()
+    })
+    test('Deve renderizar uma lista vazia', () => {
+        mockedUseListaDeParticipantes.mockReturnValue([])
+        render(<RecoilRoot><ListaParticipantes /></RecoilRoot>)
+        const lista = screen.queryAllByRole('listitem')
+        expect(lista).toHaveLength(0)
+    })
+    test('Deve renderizar uma lista com participantes', () => {
+        const participantes = ['Amanda', 'Melk', 'André', 'Beatriz', 'Daniela', 'Matheus', 'Jônatas', 'Kamille']
+        mockedUseListaDeParticipantes.mockReturnValue(participantes)
+        render(<RecoilRoot><ListaParticipantes /></RecoilRoot>)
+        const lista = screen.queryAllByRole('listitem')
+        expect(lista).toHaveLength(participantes.length)
     })
 })
