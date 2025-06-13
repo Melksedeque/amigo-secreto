68f46248ac20ea8a6121c2941e1e278cf843ff5f
Melksedeque <freelancer@melksedeque.com.br>
2025-06-12 21:33:54 -0300

    Criação de teste de botão desabilitado caso não haja participantes o suficiente

---
+++ src/components/Jogar/Jogar.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
+import { describe, expect, test } from "vitest";
+import Jogar from ".";
+
+describe('Quando não existem participantes suficientes', () => {
+    test('a brincadeira não pode ser iniciada', () => {
+        render(<RecoilRoot><Jogar /></RecoilRoot>)
+        const botao = screen.getByRole('button')
+        expect(botao).toBeDisabled()
+    })
+})
