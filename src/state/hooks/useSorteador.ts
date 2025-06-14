ff4255d3a688d9a373e0a4890bcbd41ec8a13a52
Melksedeque <freelancer@melksedeque.com.br>
2025-06-14 14:06:56 -0300

    Desenvolvimento da lógica de sorteio e inserção do resultado no atomo

--- src/state/hooks/useSorteador.ts
+++ src/state/hooks/useSorteador.ts
@@ -1,8 +1,11 @@
 import shuffle from "just-shuffle"
 import { useListaDeParticipantes } from "./useListaDeParticipantes"
+import { useSetRecoilState } from "recoil"
+import { resultadoAmigoSecreto } from "state/atom"
 
 export const useSorteador = () => {
     const participantes = useListaDeParticipantes()
+    const setResultado = useSetRecoilState(resultadoAmigoSecreto)
     return () => {
         const totalDeParticipantes = participantes.length
         const embaralhado = shuffle(participantes)
@@ -12,5 +15,7 @@ export const useSorteador = () => {
             const indiceAmigo = index === (totalDeParticipantes - 1) ? 0 : index + 1
             resultado.set(embaralhado[index], embaralhado[indiceAmigo])
         }
+
+        setResultado(resultado)
     }
 }
