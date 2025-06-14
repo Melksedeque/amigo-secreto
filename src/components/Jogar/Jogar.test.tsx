68ca1ea66e8478c5112d06776622a5620496e92a
Melksedeque <freelancer@melksedeque.com.br>
2025-06-13 21:47:36 -0300

    Criação de testes ao clicar no botão e iniciar a brincadeira

--- src/components/Jogar/Jogar.test.tsx
+++ src/components/Jogar/Jogar.test.tsx
@@ -1,15 +1,25 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
 import { describe, expect, test, vitest, type MockedFunction } from "vitest";
 import Jogar from ".";
 import { beforeEach } from "node:test";
 import { useListaDeParticipantes } from "hooks/useListaDeParticipantes";
+import { useNavigate } from "react-router-dom";
 
 vitest.mock('hooks/useListaDeParticipantes', () => {
     return {
         useListaDeParticipantes: vitest.fn()
     }
 })
+
+vitest.mock('react-router-dom', () => {
+    return {
+        useNavigate: () => vitest.fn()
+    }
+})
+
+const mockedUseNavigate = useNavigate as MockedFunction<typeof useNavigate>
+
 const mockedUseListaDeParticipantes = useListaDeParticipantes as MockedFunction<typeof useListaDeParticipantes>
 
 describe('Quando não existem participantes suficientes', () => {
@@ -34,5 +44,14 @@ describe('Quando existem participantes suficientes', () => {
         render(<RecoilRoot><Jogar /></RecoilRoot>)
         const botao = screen.getByRole('button')
         expect(botao).toBeEnabled()
+    })
+    test('ao clicar no botão, a brincadeira é iniciada', () => {
+        const participantes = ['Amanda', 'Melk', 'André', 'Beatriz', 'Daniela', 'Matheus', 'Jônatas', 'Kamille']
+        mockedUseListaDeParticipantes.mockReturnValue(participantes)
+        render(<RecoilRoot><Jogar /></RecoilRoot>)
+        const botao = screen.getByRole('button')
+        fireEvent.click(botao)
+        expect(mockedUseNavigate).toHaveBeenCalledTimes(1)
+        expect(mockedUseNavigate).toHaveBeenCalledWith('/sorteio')
     })
 })
