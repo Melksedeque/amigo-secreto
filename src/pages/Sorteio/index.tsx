ef8ad46a273b823800c32b8bd68e282cec223d89
Melksedeque <freelancer@melksedeque.com.br>
2025-06-14 13:57:30 -0300

    Criação de select para exibir os participantes

--- src/pages/Sorteio/index.tsx
+++ src/pages/Sorteio/index.tsx
@@ -1,9 +1,16 @@
+import { useListaDeParticipantes } from 'hooks/useListaDeParticipantes'
 import styles from './Sorteio.module.scss'
 
 export default function Sorteio() {
+  const participantes = useListaDeParticipantes()
   return (
     <div className={styles.container}>
       <h1>Sorteio</h1>
+      <form>
+        <select name="participanteDaVez" id="participanteDaVez">
+          {participantes.map((participante) => <option key={participante} id={participante} value={participante}>{participante}</option>)}
+        </select>
+      </form>
     </div>
   )
 }
