ad4f04dc2ca53f56937d107a9ef0299ff3904d08
Melksedeque <freelancer@melksedeque.com.br>
2025-06-14 14:39:52 -0300

    Primeiro teste de realização do sorteio para amigo secreto não ser igual ao participante

---
+++ src/state/helpers/realizarSorteio.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from "vitest";
+
+describe('Dado um sorteio de Amigo Secreto', () => {
+    test('cada participante não sorteie o próprio nome', () => {
+        const participantes = [
+            'Amanda',
+            'André',
+            'Beatriz',
+            'Jonatas',
+            'Kamille',
+            'Klau-klau',
+            'Melk',
+            'Ruth'
+        ]
+        const sorteio = realizarSorteio(participantes)
+
+        participantes.forEach(participante => {
+            const amigoSecreto = sorteio.get(participante)
+            expect(amigoSecreto).not.toEqual(participante)
+        })
+    })
+})
