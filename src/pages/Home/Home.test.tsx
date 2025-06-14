6101ed3cb9ea3a015a7540487115a847b7f637a2
Melksedeque <freelancer@melksedeque.com.br>
2025-06-13 22:30:33 -0300

    Limpeza de codigo não utilizado do navigate no teste da Home

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,17 +1,13 @@
-import { describe, expect, MockedFunction, test, vitest } from "vitest";
+import { describe, expect, test, vitest } from "vitest";
 import Home from ".";
 import { RecoilRoot } from "recoil";
 import { render } from "@testing-library/react";
-import { useNavigate } from "react-router-dom";
 
 vitest.mock('react-router-dom', () => {
     return {
         useNavigate: vitest.fn()
     }
 })
-
-const mockNavigate = vitest.fn()
-const mockedUseNavigate = useNavigate as MockedFunction<typeof useNavigate>
 
 describe('A página de configuração', () => {
   test('Deve ser renderizada corretamente', () => {
