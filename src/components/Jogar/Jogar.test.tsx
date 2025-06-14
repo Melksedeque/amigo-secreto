2fc90ca2c1dd512f93c76e0e5f42c95e02929614
Melksedeque <freelancer@melksedeque.com.br>
2025-06-13 21:21:27 -0300

    Criação de dados mockados para realização de testes com lista de participantes

--- src/components/Jogar/Jogar.test.tsx
+++ src/components/Jogar/Jogar.test.tsx
@@ -1,10 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import { RecoilRoot } from "recoil";
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vitest, type MockedFunction } from "vitest";
 import Jogar from ".";
+import { beforeEach } from "node:test";
+import { useListaDeParticipantes } from "hooks/useListaDeParticipantes";
+
+vitest.mock('hooks/useListaDeParticipantes', () => {
+    return {
+        useListaDeParticipantes: vitest.fn()
+    }
+})
+const mockedUseListaDeParticipantes = useListaDeParticipantes as MockedFunction<typeof useListaDeParticipantes>
 
 describe('Quando não existem participantes suficientes', () => {
+    beforeEach(() => {
+        mockedUseListaDeParticipantes.mockClear()
+    })
     test('a brincadeira não pode ser iniciada', () => {
+        mockedUseListaDeParticipantes.mockReturnValue([])
         render(<RecoilRoot><Jogar /></RecoilRoot>)
         const botao = screen.getByRole('button')
         expect(botao).toBeDisabled()
@@ -12,7 +25,12 @@ describe('Quando não existem participantes suficientes', () => {
 })
 
 describe('Quando existem participantes suficientes', () => {
+    beforeEach(() => {
+        mockedUseListaDeParticipantes.mockClear()
+    })
     test('a brincadeira pode ser iniciada', () => {
+        const participantes = ['Amanda', 'Melk', 'André', 'Beatriz', 'Daniela', 'Matheus', 'Jônatas', 'Kamille']
+        mockedUseListaDeParticipantes.mockReturnValue(participantes)
         render(<RecoilRoot><Jogar /></RecoilRoot>)
         const botao = screen.getByRole('button')
         expect(botao).toBeEnabled()
