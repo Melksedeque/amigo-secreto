6d184917b05ce47ff871326b6cda34291b86d239
Melksedeque <freelancer@melksedeque.com.br>
2025-06-13 21:47:09 -0300

    Criação de navegação com useNavigate para encaminhar para /sorteio

--- src/components/Jogar/index.tsx
+++ src/components/Jogar/index.tsx
@@ -1,11 +1,21 @@
 import { useListaDeParticipantes } from 'hooks/useListaDeParticipantes'
 import styles from './Jogar.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 export default function Jogar () {
     const listaDeParticipantes = useListaDeParticipantes()
+    const navigate = useNavigate()
+    const iniciar = () => {
+        navigate('/sorteio')
+    }
     return (
         <div className={styles.jogar}>
-            <button disabled={listaDeParticipantes.length < 3}>Iniciar brincadeira!</button>
+            <button
+                disabled={listaDeParticipantes.length < 3}
+                onClick={iniciar}
+            >
+                Iniciar brincadeira!
+            </button>
         </div>
     )
 }
