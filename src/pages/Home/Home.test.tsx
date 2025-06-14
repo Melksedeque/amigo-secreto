f3915d0b8274b5bb9a5377459af26f4c3a19c707
Melksedeque <freelancer@melksedeque.com.br>
2025-06-13 22:28:28 -0300

    Inclusão de useNavigate para realização do teste da página principal

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,7 +1,17 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, MockedFunction, test, vitest } from "vitest";
 import Home from ".";
 import { RecoilRoot } from "recoil";
 import { render } from "@testing-library/react";
+import { useNavigate } from "react-router-dom";
+
+vitest.mock('react-router-dom', () => {
+    return {
+        useNavigate: vitest.fn()
+    }
+})
+
+const mockNavigate = vitest.fn()
+const mockedUseNavigate = useNavigate as MockedFunction<typeof useNavigate>
 
 describe('A página de configuração', () => {
   test('Deve ser renderizada corretamente', () => {
