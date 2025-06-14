b732f29945a712cf56f4c6795b59be29e98f406e
Melksedeque <freelancer@melksedeque.com.br>
2025-06-14 14:52:48 -0300

    Chamar função de sorteio

--- src/components/Jogar/index.tsx
+++ src/components/Jogar/index.tsx
@@ -1,11 +1,14 @@
 import { useListaDeParticipantes } from 'hooks/useListaDeParticipantes'
 import styles from './Jogar.module.scss'
 import { useNavigate } from 'react-router-dom'
+import { useSorteador } from 'hooks/useSorteador'
 
 export default function Jogar () {
     const listaDeParticipantes = useListaDeParticipantes()
     const navigate = useNavigate()
+    const sortear = useSorteador()
     const iniciar = () => {
+        sortear()
         navigate('/sorteio')
     }
     return (
