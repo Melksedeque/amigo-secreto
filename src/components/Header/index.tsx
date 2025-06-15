c77529a84cf91b59aaacdc64e18670c2f81945e4
Melksedeque <freelancer@melksedeque.com.br>
2025-06-15 14:16:11 -0300

    Estrutura HTML do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,7 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <Logo />
+      <img src="/images/participante.png" alt="" className={styles.imgParticipante} />
     </header>
   )
 }
