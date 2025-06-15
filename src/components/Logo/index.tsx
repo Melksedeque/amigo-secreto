97e39004b2168981255a99ba847f6c03ea0b74d5
Melksedeque <freelancer@melksedeque.com.br>
2025-06-15 14:35:03 -0300

    Componente de Logo com as duas versões

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,16 @@ import styles from './Logo.module.scss'
 export default function Logo() {
   return (
     <div className={styles.logo}>
-      <h1><img src="/images/logo-pequeno.png" alt="Logo do Sorteador de Amigo Secreto" /></h1>
+      <img 
+        src="/images/logo.png" 
+        alt="Logo do Sorteador de Amigo Secreto" 
+        className={styles.logoDesktop}
+      />
+      <img 
+        src="/images/logo-pequeno.png" 
+        alt="Logo do Sorteador de Amigo Secreto" 
+        className={styles.logoMobile}
+      />
     </div>
   )
 }
