ecf2c46e94a0fbf58f12647f11ed4b81ef332cf5
Melksedeque <freelancer@melksedeque.com.br>
2025-06-16 13:08:43 -0300

    Remoção de HTML do Footer para importar o componente

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import styles from './Home.module.scss'
 import Header from 'components/Header'
 import ListaParticipantes from 'components/ListaParticipantes'
 import Card from 'components/Card'
+import Footer from 'components/Footer'
 
 export default function Home() {
   return (
@@ -14,44 +15,7 @@ export default function Home() {
           <ListaParticipantes />
         </Card>
       </div>
-      <footer>
-        <div>
-          <a
-            href="https://www.linkedin.com/in/melksedeque-silva/"
-            target="_blank"
-          >
-            LinkedIn
-          </a>
-          <a href="http://github.com/Melksedeque" target="_blank">
-            GitHub
-          </a>
-          <a href="https://x.com/SouzaMelk" target="_blank">
-            Twitter
-          </a>
-        </div>
-        <div>
-          <p>
-            Desenhado por{' '}
-            <a
-              href="http://www.alura.com.br/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Alura
-            </a>
-          </p>
-          <p>
-            Desenvolvido por{' '}
-            <a
-              href="http://github.com/Melksedeque"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Melksedeque
-            </a>
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </>
   )
 }
