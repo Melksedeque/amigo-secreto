3e3f1d9a0a3fb83143ea10d8b8495dd2bf839e50
Melksedeque <freelancer@melksedeque.com.br>
2025-06-16 13:56:39 -0300

    Adição de sacolas do layout

--- src/components/Jogar/index.tsx
+++ src/components/Jogar/index.tsx
@@ -19,6 +19,7 @@ export default function Jogar () {
             >
                 Iniciar brincadeira!
             </button>
+            <img src="/images/sacolas.png" alt="" />
         </div>
     )
 }
