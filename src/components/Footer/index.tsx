68404b57b353724aae40463adb274cb970c4ca29
Melksedeque <freelancer@melksedeque.com.br>
2025-06-16 13:33:14 -0300

    Estrutura dos links sociais

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,41 @@
+import styles from './Footer.module.scss'
+import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa'
+
+const iconProps = {
+  size: 24,
+  color: '#fff'
+}
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+        <div className={styles.socialLinks}>
+          <FaLinkedin {...iconProps} to="https://www.linkedin.com/in/melksedeque-silva/" target='_blank' />
+          <FaGithub {...iconProps} to="http://github.com/Melksedeque" target='_blank' />
+          <FaTwitter {...iconProps} to="https://x.com/SouzaMelk" target='_blank' />
+        </div>
+        <div>
+          <p>
+            Desenhado por{' '}
+            <a
+              href="http://www.alura.com.br/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Alura
+            </a>
+          </p>
+          <p>
+            Desenvolvido por{' '}
+            <a
+              href="http://github.com/Melksedeque"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Melksedeque
+            </a>
+          </p>
+        </div>
+      </footer>
+  )
+}
