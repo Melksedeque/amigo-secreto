329403e323c8e0a9a38b68cd185ca37f5c6fe814
Melksedeque <freelancer@melksedeque.com.br>
2025-06-18 15:11:40 -0300

    Link para Home

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,18 +1,21 @@
+import { Link } from 'react-router-dom'
 import styles from './Logo.module.scss'
 
 export default function Logo() {
   return (
     <div className={styles.logo}>
-      <img 
-        src="/images/logo.png" 
-        alt="Logo do Sorteador de Amigo Secreto" 
-        className={styles.logoDesktop}
-      />
-      <img 
-        src="/images/logo-pequeno.png" 
-        alt="Logo do Sorteador de Amigo Secreto" 
-        className={styles.logoMobile}
-      />
+      <Link to="/">
+        <img 
+          src="/images/logo.png" 
+          alt="Logo do Sorteador de Amigo Secreto" 
+          className={styles.logoDesktop}
+        />
+        <img 
+          src="/images/logo-pequeno.png" 
+          alt="Logo do Sorteador de Amigo Secreto" 
+          className={styles.logoMobile}
+        />
+      </Link>
     </div>
   )
 }
