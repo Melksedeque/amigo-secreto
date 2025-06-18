70ec680c11637e7c290ab35aa40c19abf28a349d
Melksedeque <freelancer@melksedeque.com.br>
2025-06-18 14:49:55 -0300

    Remoção de div não utilizada de de import de CSS não usado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import Formulario from 'components/Formulario'
-import styles from './Home.module.scss'
 import Header from 'components/Header'
 import ListaParticipantes from 'components/ListaParticipantes'
 import Card from 'components/Card'
@@ -10,14 +9,12 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className={styles.container}>
         <Card>
           <h2>Vamos começar!</h2>
           <Formulario /> 
           <ListaParticipantes />
           <Jogar />
         </Card>
-      </div>
       <Footer />
     </>
   )
