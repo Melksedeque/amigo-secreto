e5b6f4e7c19aedc20e87ada0c5197d11c25fc7d6
Melksedeque <freelancer@melksedeque.com.br>
2025-06-18 14:41:44 -0300

    Remover titulo do componente Card e passada a responsabilidade para a página adequadamente

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,7 +3,6 @@ import styles from './Card.module.scss'
 export default function Card({children}: React.PropsWithChildren) {
     return (
         <div className={styles.card}>
-            <h2>Vamos começar!</h2>
             {children}
         </div>
     )
