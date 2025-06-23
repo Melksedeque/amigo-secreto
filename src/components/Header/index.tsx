f194e89ed40515a1bfa786fd98bff83011450147
Melksedeque <freelancer@melksedeque.com.br>
2025-06-23 19:58:16 -0300

    Arquivo de Componente Header formatado com Prettier

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import Logo from 'components/Logo'
-import styles from './Header.module.scss'
+import Logo from 'components/Logo';
+import styles from './Header.module.scss';
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Logo />
-      <img src="/images/participante.png" alt="" className={styles.imgParticipante} />
+      <img
+        src="/images/participante.png"
+        alt=""
+        className={styles.imgParticipante}
+      />
     </header>
-  )
+  );
 }
