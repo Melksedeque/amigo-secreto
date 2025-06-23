9f5761275e4e51e5fd140ebfbd926f5b430dd9d7
Melksedeque <freelancer@melksedeque.com.br>
2025-06-23 19:59:00 -0300

    Arquivo de Componente Logo formatado com Prettier

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,21 +1,21 @@
-import { Link } from 'react-router-dom'
-import styles from './Logo.module.scss'
+import { Link } from 'react-router-dom';
+import styles from './Logo.module.scss';
 
 export default function Logo() {
   return (
     <div className={styles.logo}>
       <Link to="/">
-        <img 
-          src="/images/logo.png" 
-          alt="Logo do Sorteador de Amigo Secreto" 
+        <img
+          src="/images/logo.png"
+          alt="Logo do Sorteador de Amigo Secreto"
           className={styles.logoDesktop}
         />
-        <img 
-          src="/images/logo-pequeno.png" 
-          alt="Logo do Sorteador de Amigo Secreto" 
+        <img
+          src="/images/logo-pequeno.png"
+          alt="Logo do Sorteador de Amigo Secreto"
           className={styles.logoMobile}
         />
       </Link>
     </div>
-  )
+  );
 }
