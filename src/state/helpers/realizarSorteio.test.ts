f25d7dd587f7328f3247ebfc41b5d94240f64b69
Melksedeque <freelancer@melksedeque.com.br>
2025-06-23 20:01:08 -0300

    Arquivo de testes do helper de realizar sorteio

--- src/state/helpers/realizarSorteio.test.ts
+++ src/state/helpers/realizarSorteio.test.ts
@@ -1,23 +1,23 @@
-import { describe, expect, test } from "vitest";
-import { realizarSorteio } from "./realizarSorteio";
+import { describe, expect, test } from 'vitest';
+import { realizarSorteio } from './realizarSorteio';
 
 describe('Dado um sorteio de Amigo Secreto', () => {
-    test('cada participante não sorteie o próprio nome', () => {
-        const participantes = [
-            'Amanda',
-            'André',
-            'Beatriz',
-            'Jonatas',
-            'Kamille',
-            'Klau-klau',
-            'Melk',
-            'Ruth'
-        ]
-        const sorteio = realizarSorteio(participantes)
+  test('cada participante não sorteie o próprio nome', () => {
+    const participantes = [
+      'Amanda',
+      'André',
+      'Beatriz',
+      'Jonatas',
+      'Kamille',
+      'Klau-klau',
+      'Melk',
+      'Ruth',
+    ];
+    const sorteio = realizarSorteio(participantes);
 
-        participantes.forEach(participante => {
-            const amigoSecreto = sorteio.get(participante)
-            expect(amigoSecreto).not.toEqual(participante)
-        })
-    })
-})
+    participantes.forEach((participante) => {
+      const amigoSecreto = sorteio.get(participante);
+      expect(amigoSecreto).not.toEqual(participante);
+    });
+  });
+});
