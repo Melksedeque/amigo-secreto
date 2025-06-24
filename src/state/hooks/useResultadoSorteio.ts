797775d2e73ee41391c990e7de50ea294c52cd0c
Melksedeque <freelancer@melksedeque.com.br>
2025-06-24 15:57:03 -0300

    Estilização do Hook useResultadoSorteio com o Prettier

--- src/state/hooks/useResultadoSorteio.ts
+++ src/state/hooks/useResultadoSorteio.ts
@@ -1,6 +1,6 @@
-import { useRecoilValue } from "recoil"
-import { resultadoAmigoSecreto } from "state/atom"
+import { useRecoilValue } from 'recoil';
+import { resultadoAmigoSecreto } from 'state/atom';
 
 export const useResultadoSorteio = () => {
-    return useRecoilValue(resultadoAmigoSecreto)
-}
+  return useRecoilValue(resultadoAmigoSecreto);
+};
