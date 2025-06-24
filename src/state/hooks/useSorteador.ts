185af5b9382e7a65af15df30cfa7a97b6dc2ef7c
Melksedeque <freelancer@melksedeque.com.br>
2025-06-24 15:57:16 -0300

    Estilização do Hook useSorteador com o Prettier

--- src/state/hooks/useSorteador.ts
+++ src/state/hooks/useSorteador.ts
@@ -1,13 +1,13 @@
-import { useListaDeParticipantes } from "./useListaDeParticipantes"
-import { useSetRecoilState } from "recoil"
-import { resultadoAmigoSecreto } from "state/atom"
-import { realizarSorteio } from "state/helpers/realizarSorteio"
+import { useListaDeParticipantes } from './useListaDeParticipantes';
+import { useSetRecoilState } from 'recoil';
+import { resultadoAmigoSecreto } from 'state/atom';
+import { realizarSorteio } from 'state/helpers/realizarSorteio';
 
 export const useSorteador = () => {
-    const participantes = useListaDeParticipantes()
-    const setResultado = useSetRecoilState(resultadoAmigoSecreto)
-    return () => {
-        const resultado = realizarSorteio(participantes)
-        setResultado(resultado)
-    }
-}
+  const participantes = useListaDeParticipantes();
+  const setResultado = useSetRecoilState(resultadoAmigoSecreto);
+  return () => {
+    const resultado = realizarSorteio(participantes);
+    setResultado(resultado);
+  };
+};
