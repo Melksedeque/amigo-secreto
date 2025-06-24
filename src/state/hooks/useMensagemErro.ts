9c8873fa5b6fd7eb287ac85538440ca5820870e7
Melksedeque <freelancer@melksedeque.com.br>
2025-06-24 15:56:41 -0300

    Estilização do Hook useMensagemErro com o Prettier

--- src/state/hooks/useMensagemErro.ts
+++ src/state/hooks/useMensagemErro.ts
@@ -1,6 +1,6 @@
-import { useRecoilValue } from "recoil"
-import { erroState } from "state/atom"
+import { useRecoilValue } from 'recoil';
+import { erroState } from 'state/atom';
 
 export const useMensagemErro = () => {
-  return useRecoilValue(erroState)
-}
+  return useRecoilValue(erroState);
+};
