3429bb881ac11ed6c6fa510a3928e4d0aee86dfa
Melksedeque <freelancer@melksedeque.com.br>
2025-06-24 15:53:01 -0300

    Estilização do Hook useAdicionarParticipantes com o Prettier

--- src/state/hooks/useAdicionarParticipante.ts
+++ src/state/hooks/useAdicionarParticipante.ts
@@ -1,25 +1,25 @@
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { erroState, listaParticipantesState } from "../atom";
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { erroState, listaParticipantesState } from '../atom';
 
 export const useAdicionarParticipante = () => {
-    const setLista = useSetRecoilState(listaParticipantesState);
-    const lista = useRecoilValue(listaParticipantesState);
-    const setErro = useSetRecoilState(erroState);
-    return (nomeDoParticipante: string) => {
-        if (nomeDoParticipante.length > 3) {
-            if(lista.includes(nomeDoParticipante)) {
-                setErro("Nomes duplicados não são permitidos!");
-                setTimeout(() => {
-                    setErro("");
-                }, 5000);
-                return;
-            }
-            return setLista(listaAtual => [...listaAtual, nomeDoParticipante]);
-        } else {
-            setErro("Nome do participante precisa ter mais de 3 caracteres");
-            setTimeout(() => {
-                setErro("");
-            }, 5000);
-        }
+  const setLista = useSetRecoilState(listaParticipantesState);
+  const lista = useRecoilValue(listaParticipantesState);
+  const setErro = useSetRecoilState(erroState);
+  return (nomeDoParticipante: string) => {
+    if (nomeDoParticipante.length > 3) {
+      if (lista.includes(nomeDoParticipante)) {
+        setErro('Nomes duplicados não são permitidos!');
+        setTimeout(() => {
+          setErro('');
+        }, 5000);
+        return;
+      }
+      return setLista((listaAtual) => [...listaAtual, nomeDoParticipante]);
+    } else {
+      setErro('Nome do participante precisa ter mais de 3 caracteres');
+      setTimeout(() => {
+        setErro('');
+      }, 5000);
     }
-}
+  };
+};
